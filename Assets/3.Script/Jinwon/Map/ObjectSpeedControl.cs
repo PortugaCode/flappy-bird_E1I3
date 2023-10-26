f5b45de1ac8fc64a582a2e26749b43c4883398a5
Jinwon Kim <jinwon0707@gmail.com>
2023-10-26 17:16:12 +0900

    [Fix] Add Annotation
    스크립트 주석 작성

--- Assets/3.Script/Jinwon/Map/ObjectSpeedControl.cs
+++ Assets/3.Script/Jinwon/Map/ObjectSpeedControl.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class ObjectSpeedControl : MonoBehaviour
 {
-    public void SetObjectSpeed(float speed)
+    public void SetObjectSpeed(float speed) // 오브젝트풀에 저장된 오브젝트들의 이동속도 조정
     {
-        for (int i = 0; i < transform.childCount; i++)
+        for (int i = 0; i < transform.childCount; i++) // 오브젝트풀의 모든  자식 오브젝트들
         {
-            if (transform.GetChild(i).TryGetComponent(out Ground ground))
+            if (transform.GetChild(i).TryGetComponent(out Ground ground)) // ground 오브젝트라면 ground 속도 조정
             {
                 ground.moveSpeed = speed;
             }
-            else if (transform.GetChild(i).TryGetComponent(out Pipe pipe))
+            else if (transform.GetChild(i).TryGetComponent(out Pipe pipe)) // pipe 오브젝트라면 pipe 속도 조정
             {
                 pipe.moveSpeed = speed;
             }
--- Assets/3.Script/Jinwon/Map/Pipe.cs
+++ Assets/3.Script/Jinwon/Map/Pipe.cs
@@ -5,39 +5,39 @@ using UnityEngine;
 public class Pipe : MonoBehaviour
 {
     [Header("Pipe")]
-    [SerializeField] private GameObject upper;
-    [SerializeField] private GameObject lower;
+    [SerializeField] private GameObject upper; // 위 파이프
+    [SerializeField] private GameObject lower; // 아래 파이프
 
-    private float middlePosition;
+    private float middlePosition; // 중간에 파이프 빈 곳의 위치
 
-    private float upperScale;
-    private float lowerScale;
+    private float upperScale; // 위 파이프의 스케일
+    private float lowerScale; // 아래 파이프의 스케일
 
     [Header("Status")]
-    public float moveSpeed = 5.0f;
+    public float moveSpeed = 5.0f; // 파이프 이동 속도
 
-    private bool isEnd = false;
+    private bool isEnd = false; // 끝까지 갔는지 확인 할 변수
 
     private void Update()
     {
-        MoveForward();
-        CheckEnd();
+        MoveForward(); // 파이프 이동
+        CheckEnd(); // 끝까지 갔는지 확인 할 메서드
     }
 
-    public void SetRandomShape()
+    public void SetRandomShape() // 파이프 빈 곳의 위치 랜덤 설정
     {
         isEnd = false;
 
-        middlePosition = Random.Range(-3.0f, 3.0f);
+        middlePosition = Random.Range(-3.0f, 3.0f); // 빈 곳 -3.0 ~ 3.0 중 랜덤 설정
 
-        upperScale = 6.0f - middlePosition * 1.2f + Mathf.Pow(middlePosition * 0.025f, 2);
-        lowerScale = 6.0f + middlePosition * 1.2f - Mathf.Pow(middlePosition * 0.025f, 2);
+        upperScale = 6.0f - middlePosition * 1.2f + Mathf.Pow(middlePosition * 0.025f, 2); // 위 파이프 스케일 계산
+        lowerScale = 6.0f + middlePosition * 1.2f - Mathf.Pow(middlePosition * 0.025f, 2); // 아래 파이프 스케일 계산
 
-        upper.transform.localScale = new Vector3(upper.transform.localScale.x, upperScale, upper.transform.localScale.z);
-        lower.transform.localScale = new Vector3(lower.transform.localScale.x, lowerScale, lower.transform.localScale.z);
+        upper.transform.localScale = new Vector3(upper.transform.localScale.x, upperScale, upper.transform.localScale.z); // 위 파이프 스케일 설정
+        lower.transform.localScale = new Vector3(lower.transform.localScale.x, lowerScale, lower.transform.localScale.z); // 아래 파이프 스케일 설정
     }
 
-    public void ClosePipe()
+    public void ClosePipe() // 해당 파이프에 빈 곳이 없게 스케일 설정
     {
         upper.transform.localScale = new Vector3(upper.transform.localScale.x, 7.5f, upper.transform.localScale.z);
         lower.transform.localScale = new Vector3(lower.transform.localScale.x, 7.5f, lower.transform.localScale.z);
@@ -45,12 +45,12 @@ public class Pipe : MonoBehaviour
 
     private void MoveForward()
     {
-        transform.position += new Vector3(0, 0, -1.0f * moveSpeed * Time.deltaTime);
+        transform.position += new Vector3(0, 0, -1.0f * moveSpeed * Time.deltaTime); // 파이프를 -z 축으로 이동
     }
 
     private void CheckEnd()
     {
-        if (transform.position.z < -17.5f && !isEnd)
+        if (transform.position.z < -17.5f && !isEnd) // 끝까지 갔으면 비활성화 후 큐에 넣기
         {
             isEnd = true;
             gameObject.SetActive(false);
